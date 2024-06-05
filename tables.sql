0794996e0e56c7236cb3d399de1e842f426a9986
Lucinda Jordaan <Lucinda.Botes@bbd.co.za>
2024-06-05 17:57:12 +0200

    Add liquibase

--- tables.sql
+++ tables.sql
@@ -1,20 +1,27 @@
+--liquibase formatted sql
+
+--changeset ljordaan:create-users-table
 CREATE TABLE "Users" (
     "userId" SERIAL PRIMARY KEY,
     "userProfilePicture" TEXT,
     "email" TEXT UNIQUE NOT NULL,
     "isDeleted" BOOLEAN DEFAULT FALSE
 );
+-- rollback DROP TABLE "Users";
 
+--changeset ljordaan:create-boards-table
 CREATE TABLE "Boards" (
     "boardId" SERIAL PRIMARY KEY,
     "userId" INTEGER REFERENCES "Users"("userId"),
     "boardCollaborators" INTEGER[],
     "boardName" TEXT,
     "boardCreateDate" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    "isPublic" BOOLEAN DEFAULT FALSE, --What is the default value for this? false for security
+    "isPublic" BOOLEAN DEFAULT FALSE,
     "isDeleted" BOOLEAN DEFAULT FALSE
 );
+-- rollback DROP TABLE "Boards";
 
+--changeset ljordaan:create-lists-table
 CREATE TABLE "Lists" (
     "listId" SERIAL PRIMARY KEY,
     "userId" INTEGER REFERENCES "Users"("userId"),
@@ -23,7 +30,9 @@ CREATE TABLE "Lists" (
     "listCreateDate" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     "isDeleted" BOOLEAN DEFAULT FALSE
 );
+-- rollback DROP TABLE "Lists";
 
+--changeset ljordaan:create-tickets-table
 CREATE TABLE "Tickets" (
     "ticketId" SERIAL PRIMARY KEY,
     "userId" INTEGER REFERENCES "Users"("userId"),
@@ -36,3 +45,4 @@ CREATE TABLE "Tickets" (
     "ticketDueDate" TIMESTAMP,
     "isDeleted" BOOLEAN DEFAULT FALSE
 );
+-- rollback DROP TABLE "Tickets";
